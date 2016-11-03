b5bd564dcaedeae1aefce0ef7c7a60afbdcb9082
ungdungdoan <ungdungdoan@gmail.com>
2016-11-04 00:35:34 +0700

    Delete show.cs

--- show_Gridview_table/show.cs
+++
@@ -1,7 +0,0 @@
-﻿  public void laybangkhachhang()
-        {
-            Ketnoi kn = new Ketnoi();
-            DataTable dt = new DataTable();
-            dt = kn.laybang("Select * from KHACH_HANG");
-            GridViewSPBT.DataSource = dt;
-        }
