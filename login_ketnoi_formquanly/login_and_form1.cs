9212274f740b52c638106dfea625339747d47686
ungdungdoan <ungdungdoan@gmail.com>
2016-11-04 00:26:40 +0700

    Add files via upload

---
+++ login_ketnoi_formquanly/login_and_form1.cs
@@ -0,0 +1,8 @@
+﻿  private void Form1_Load(object sender, EventArgs e)
+        {
+            login frm = new login();
+            frm.Show();
+            Hide();
+
+            laybangkhachhang();
+        }
