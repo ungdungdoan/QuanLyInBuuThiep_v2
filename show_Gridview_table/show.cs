f3eabccfe9aba7c9e3ed42468ba1c9c313f19a98
ungdungdoan <ungdungdoan@gmail.com>
2016-11-04 00:30:31 +0700

    Add files via upload

---
+++ show_Gridview_table/show.cs
@@ -0,0 +1,7 @@
+﻿  public void laybangkhachhang()
+        {
+            Ketnoi kn = new Ketnoi();
+            DataTable dt = new DataTable();
+            dt = kn.laybang("Select * from KHACH_HANG");
+            GridViewSPBT.DataSource = dt;
+        }
