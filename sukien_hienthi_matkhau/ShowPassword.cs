15903a360805da52a9b4fcfc7622e55af4a1f674
ungdungdoan <ungdungdoan@gmail.com>
2016-11-04 00:25:51 +0700

    Add files via upload

---
+++ sukien_hienthi_matkhau/ShowPassword.cs
@@ -0,0 +1,7 @@
+﻿ private void ckbshow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckbshow.Checked)
+            { btnmk.UseSystemPasswordChar = false; }
+            else
+            { btnmk.UseSystemPasswordChar = true; }
+        }
