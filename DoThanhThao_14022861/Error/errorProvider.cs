64f4eee4f0de79f9907cdef55e3e92b893e33e59
ungdungdoan <ungdungdoan@gmail.com>
2016-11-04 00:37:14 +0700

    Add files via upload

---
+++ DoThanhThao_14022861/Error/errorProvider.cs
@@ -0,0 +1,4 @@
+﻿ private void txtmakh_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(txtmakh, "");
+        }
---
+++ DoThanhThao_14022861/delete/xoa.cs
@@ -0,0 +1,23 @@
+﻿ private void btXoa_Click(object sender, EventArgs e)
+        {
+            if (txttenkh.Text == "")
+            { errorProvider1.SetError(txttenkh, "Bạn chưa nhập Tên khách hàng "); }
+            if (txtmabt.Text == "")
+            { errorProvider1.SetError(txtmabt, "Bạn chưa nhập Họ khách hàng "); }
+            if (txtmakh.Text == "")
+            { errorProvider1.SetError(txtmakh, "Bạn chưa nhập Mã khách hàng "); }
+            if (txtsdt.Text == "")
+            { errorProvider1.SetError(txtsdt, "Bạn chưa nhập sđt khách hàng "); }
+            if (txtmakh.Text != "" && txtmabt.Text != "" && txttenkh.Text != "" && txtsdt.Text != "")
+            {
+                Ketnoi kn = new Ketnoi();
+                int kq = kn.xulydulieu("delete from KHACH_HANG where makh='" + txtmakh.Text + "'");
+                if (kq > 0)
+                {
+                    MessageBox.Show("xóa thành công ");
+                    laybangkhachhang();
+                }
+                else
+                { MessageBox.Show(" không thành công , vui lòng thử lại !"); }
+            }
+        }
