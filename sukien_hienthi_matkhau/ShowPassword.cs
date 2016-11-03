708eb1a8f18bb320e18daea8699d5b7212070657
ungdungdoan <ungdungdoan@gmail.com>
2016-11-04 00:35:47 +0700

    Delete ShowPassword.cs

--- sukien_hienthi_matkhau/ShowPassword.cs
+++
@@ -1,7 +0,0 @@
-﻿ private void ckbshow_CheckedChanged(object sender, EventArgs e)
-        {
-            if (ckbshow.Checked)
-            { btnmk.UseSystemPasswordChar = false; }
-            else
-            { btnmk.UseSystemPasswordChar = true; }
-        }
