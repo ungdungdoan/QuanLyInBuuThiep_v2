49353552b47fd627ff31a5b509f3fc26fbd694d0
ungdungdoan <ungdungdoan@gmail.com>
2016-11-10 21:40:10 +0700

    Add files via upload

---
+++ nguyenthanhthien/Xoa.java
@@ -0,0 +1,19 @@
+﻿public void deleteThiep(int makh) throws SQLException{
+	String sql = "DELETE FROM SANPHAM WHERE makh=?";
+	PreparedStatement statement = con.prepareStatement(sql);
+	statement.setInt(1, makh);
+	int rowsInserted = statement.executeUpdate();
+	if (rowsInserted > 0) {
+	    System.out.println("A new user was inserted successfully!");
+	
+}
+	System.out.println("them thanh cong");
+	detroy();
+}	
+	public static void detroy() throws SQLException{
+		con.close();
+	}
+
+	
+	
+}
