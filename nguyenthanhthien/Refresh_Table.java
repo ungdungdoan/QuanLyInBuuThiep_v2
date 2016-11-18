95d9657fa9a3278b2cfa33dc3f13abed9546caed
ungdungdoan <ungdungdoan@gmail.com>
2016-11-18 23:16:11 +0700

    Add files via upload

---
+++ nguyenthanhthien/Refresh_Table.java
@@ -0,0 +1,11 @@
+﻿ public void refreshTable(){
+    	 
+            // removing all the rows of the table
+            DefaultTableModel dm = (DefaultTableModel)table.getModel();
+            dm.getDataVector().removeAllElements();
+            System.out.println("Refresh Table");
+
+            //calling method addRows
+            addRows();
+      
+      }
